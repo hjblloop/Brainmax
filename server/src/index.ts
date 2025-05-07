d1c4ce8bf7b9d3faf5d40b04e97ef3cfea7000e0
hjblloop <jonathanc.jcho456852@gmail.com>
2025-05-07 18:30:40 -0400

    added gemini

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,11 +3,11 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import pool from './db.js'; // Add `.js` extension
 import axios from 'axios';
-import { OpenAI } from 'openai';
+import { GoogleGenAI } from "@google/genai";
 
 dotenv.config();
 const apiKey = process.env.OPENAI_API_KEY;
-const client = new OpenAI({apiKey});
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 const app = express();
 app.use(cors());
@@ -17,27 +17,23 @@ const PORT = process.env.PORT || 5001;
 
 app.post('/api/ask', async (req, res) => {
     // uncomment when you decide what ai api to use
-    // try {
-    //     const expertise = req.body.expertise;
-    //     const subject = req.body.subject;
-    //     const completion = await client.chat.completions.create({
-    //         model: "gpt-3.5-turbo",
-    //         messages: [
-    //             { 
-    //                 role: "system",
-    //                 content: `You are a curious student who is a ${expertise} in ${subject}. Generate 5 questions to test your knowledge.`
-    //             },
-    //             {
-    //                 role: "user",
-    //                 content: `I am a teacher. I want to answer your questions.`
-    //             },
-    //         ],
-    //     });
-    //     res.json(completion.choices[0].message.content);
-    // } catch (error: any) {
-    //     res.status(500).json( { error: error.message });
-    // }
-    res.json("this is a temp return json message for ai api");
+    try {
+        const expertise = req.body.expertise;
+        const subject = req.body.subject;
+        const response = await ai.models.generateContent({
+            model: "gemini-2.0-flash",
+            contents: `I am a teacher. Let me answer your questions.`,
+            config: {
+                systemInstruction: `You are a curious student who is a ${expertise} in ${subject}. Generate 5 questions to test your knowledge.`,
+                maxOutputTokens: 500,
+                temperature: 0.5,
+            }
+        });
+        res.json(response.text);
+    } catch (error: any) {
+        res.status(500).json( { error: error.message });
+    }
+    //res.json("this is a temp return json message for ai api");
     // try {
     //     console.log("this is server");
     //     const messages = req.body;
